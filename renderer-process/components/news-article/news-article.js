15073b827d8da24fa14b0987d8a33f238beff215
peter.palka <peter.palka@gmail.com>
2021-05-13 18:34:45 +0200

    sesta lekce - castecny commit

--- renderer-process/components/news-article/news-article.js
+++ renderer-process/components/news-article/news-article.js
@@ -1,17 +1,15 @@
-export class NewsArticle {
-
-    createDivForNews(newsContents) {
-        const newsArticle = document.createElement('div');
-        newsArticle.classList.add('news-article');
-        newsArticle.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), transparent), url(${newsContents.image})`;
+export class NewsArticle extends HTMLElement {
+    constructor(newsContents) {
+        super();
+        this.classList.add('news-article');
+        this.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), transparent), url(${newsContents.image})`;
 
         const title = document.createElement('span');
         title.classList.add('news-article__title');
         title.innerText = newsContents.title;
 
-        newsArticle.appendChild(title);
-
-        return newsArticle;
+        this.appendChild(title);
     }
-
 }
+
+customElements.define('app-news-article', NewsArticle);
--- renderer-process/main.js
+++ renderer-process/main.js
@@ -18,9 +18,8 @@ function populateNewsCarousel(news, startAt) {
     header.innerText = '';
     for(let i = startAt; i < (startAt + carouselItemCount); i ++) {
         const newsValue = news[i];
-        const newsArticle = new NewsArticle();
-        const newsDiv = newsArticle.createDivForNews(newsValue);
-        header.appendChild(newsDiv);
+        const newsArticle = new NewsArticle(newsValue);
+        header.appendChild(newsArticle);
     }
   checkButtonsVisibility();
 }
@@ -44,9 +43,15 @@ buttonRight.addEventListener('click', () => {
     populateNewsCarousel(articles, carouselItemStart);
 });
 
+/*
+1. Vytvorit novu slozku v components 'day'
+2. Vytvorit day.js a daj.scss v novej slozke
+3. Zaregistrovat novy element prostrednictvom customElements
+4. V konstruktoru novej triedy vloz do innerText akykolvek text (len aby sme videli ci sa nieco deje)
+5. Vyselektuje section.main-content
+*/
 
 // ukazkovy kod:
-
 const text = 'ashdf';
 const cislo = 3;
 
@@ -64,12 +69,18 @@ class Operenec {
     }
 
     vydejZvuk() {
+        console.log('delka zvuku', this.zvuk.length);
         console.log(this.zvuk);
     }
 
 }
 
 class Kacer extends Operenec {
+    constructor() {
+        super();
+        console.log(this.zvuk);
+    }
+
     plavPoJezirku() {
         console.log('plavu plavu');
     }
@@ -81,12 +92,10 @@ class Kohout extends Operenec {
     }
 }
 
-const kacer = new Kacer('kva kva');
+const kacer = new Kacer();
 
 kacer.vydejZvuk();
-kacer.plavPoJezirku();
 
 const kohout = new Kohout('kikiriki');
 
 kohout.vydejZvuk();
-kohout.hlasVychodSlunce();
