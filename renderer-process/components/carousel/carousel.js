8af975aad061b808816145c2dba626db945a274b
peter.palka <peter.palka@gmail.com>
2021-05-16 10:24:12 +0200

    domaci ukol 4

--- renderer-process/components/carousel/carousel.js
+++ renderer-process/components/carousel/carousel.js
@@ -1,7 +1,21 @@
 import { NewsArticle } from '../news-article/news-article.js';
 
-export class Carousel {
+export class Carousel extends HTMLElement {
     constructor() {
+        super();
+        this.innerHTML = `
+        <header class="header-news">
+            <div class="header-news__container">
+            </div>
+            <button id="carousel-button-left">
+                <i class="fas fa-chevron-left"></i>
+            </button>
+            <button id="carousel-button-right" class="last">
+                <i class="fas fa-chevron-right"></i>
+            </button>
+        </header>
+        `;
+
         this.header = document.querySelector('header.header-news > div.header-news__container');
         this.carouselItemCount = 2;
         this.carouselItemStart = 0;
@@ -25,7 +39,7 @@ export class Carousel {
         this.articles = news;
         this.header.innerText = '';
         for(let i = this.carouselItemStart; i < (this.carouselItemStart + this.carouselItemCount); i ++) {
-            const newsValue = news[i];
+            const newsValue = this.articles[i];
             const newsArticle = new NewsArticle(newsValue);
             this.header.appendChild(newsArticle);
         }
@@ -37,3 +51,5 @@ export class Carousel {
         this.buttonRight.hidden = this.carouselItemStart >= (this.articles.length - this.carouselItemCount);
     }
 }
+
+customElements.define('app-carousel', Carousel);
--- renderer-process/main.js
+++ renderer-process/main.js
@@ -1,64 +1,16 @@
-import { Day } from './components/day/day.js';
 import { Carousel } from './components/carousel/carousel.js';
+import { Day } from './components/day/day.js';
+
+const carousel = document.querySelector('app-carousel');
 
 fetch('http://localhost:3000/news.json')
     .then(serverResponse => serverResponse.text())
     .then(responseText => {
-        const data = JSON.parse(responseText);
-        const carousel = new Carousel();
-        carousel.populateNewsCarousel(data.articles);
+      const data = JSON.parse(responseText);
+      carousel.populateNewsCarousel(data.articles);
     });
 
 const mainContent = document.querySelector('section.main-content');
 for (let i = 1; i < 31; i++) {
     mainContent.appendChild(new Day(i));
 }
-
-// ukazkovy kod:
-const text = 'ashdf';
-const cislo = 3;
-
-const automobil = {
-    pocetMistKSezeni: 5,
-    barvaKaroserie: 'cervena'
-};
-
-console.log(automobil.barvaKaroserie);
-
-class Operenec {
-
-    constructor(volani) {
-        this.zvuk = volani;
-    }
-
-    vydejZvuk() {
-        console.log('delka zvuku', this.zvuk.length);
-        console.log(this.zvuk);
-    }
-
-}
-
-class Kacer extends Operenec {
-    constructor() {
-        super('kva kva');
-        console.log(this.zvuk);
-    }
-
-    plavPoJezirku() {
-        console.log('plavu plavu');
-    }
-}
-
-class Kohout extends Operenec {
-    hlasVychodSlunce() {
-        console.log('vychazi');
-    }
-}
-
-const kacer = new Kacer();
-
-kacer.vydejZvuk();
-
-const kohout = new Kohout('kikiriki');
-
-kohout.vydejZvuk();
