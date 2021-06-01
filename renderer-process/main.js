79a92dbfdf5149a274401ecd936a1e16f42708a7
peter.palka <peter.palka@gmail.com>
2021-06-01 09:19:49 +0200

    osma lekce

--- renderer-process/main.js
+++ renderer-process/main.js
@@ -13,7 +13,6 @@ fetch('http://localhost:3000/news.json')
 const mainContent = document.querySelector('section.main-content');
 
 const currentDate = new Date();
-debugger;
 const maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
 
 for (let i = 1; i <= maxDate; i++) {
@@ -21,58 +20,88 @@ for (let i = 1; i <= maxDate; i++) {
     mainContent.appendChild(new Day(dayDate));
 }
 
-const buttonOpenModal = document.getElementById('open-modal');
-const modalConatiner = document.querySelector('.modal-container');
-
-buttonOpenModal.addEventListener('click', () => {
-    modalConatiner.hidden = false;
+const openModalButton = document.querySelector('#open-modal');
+openModalButton.addEventListener('click', () => {
+    showDayModal().then((result) => console.log(result));
 });
 
+function showDayModal() {
+    const promiseResult = new Promise((resolve, reject) => {
+        const template = document.querySelector('#modal-template');
+        const modal = template.content.cloneNode(true);
+
+        const closeAction = () => {
+            const child = document.querySelector('section.modal-container');
+            document.body.removeChild(child);
+            resolve(null);
+        };
+
+        modal.querySelector('#close-modal').addEventListener('click', closeAction);
+
+        const cancelButton = modal.querySelector('#cancel-button');
+        if (cancelButton === null) {
+            reject('Must have cancel button');
+        }
+        cancelButton.addEventListener('click', closeAction);
+
+        modal.querySelector('#save-button').addEventListener('click', () => {
+            const formRef = document.querySelector('#modal-form');
+            const formData = new FormData(formRef);
+            const isHoliday = formData.get('isHolidayControl') === 'on';
+            resolve('ahoj!!!!!');
+        });
+
+        document.body.appendChild(modal);
+    });
+
+    return promiseResult;
+}
+
 // ukazkovy kod:
-const text = 'ashdf';
-const cislo = 3;
+// const text = 'ashdf';
+// const cislo = 3;
 
-const automobil = {
-    pocetMistKSezeni: 5,
-    barvaKaroserie: 'cervena'
-};
+// const automobil = {
+//     pocetMistKSezeni: 5,
+//     barvaKaroserie: 'cervena'
+// };
 
-console.log(automobil.barvaKaroserie);
+// console.log(automobil.barvaKaroserie);
 
-class Operenec {
+// class Operenec {
 
-    constructor(volani) {
-        this.zvuk = volani;
-    }
+//     constructor(volani) {
+//         this.zvuk = volani;
+//     }
 
-    vydejZvuk() {
-        console.log('delka zvuku', this.zvuk.length);
-        console.log(this.zvuk);
-    }
+//     vydejZvuk() {
+//         console.log('delka zvuku', this.zvuk.length);
+//         console.log(this.zvuk);
+//     }
 
-}
+// }
 
-class Kacer extends Operenec {
-    constructor() {
-        super('kva kva');
-        console.log(this.zvuk);
-    }
+// class Kacer extends Operenec {
+//     constructor() {
+//         super('kva kva');
+//         console.log(this.zvuk);
+//     }
 
-    plavPoJezirku() {
-        console.log('plavu plavu');
-    }
-}
+//     plavPoJezirku() {
+//         console.log('plavu plavu');
+//     }
+// }
 
-class Kohout extends Operenec {
-    hlasVychodSlunce() {
-        console.log('vychazi');
-    }
-}
+// class Kohout extends Operenec {
+//     hlasVychodSlunce() {
+//         console.log('vychazi');
+//     }
+// }
 
-const kacer = new Kacer();
+// const kacer = new Kacer();
 
-kacer.vydejZvuk();
+// kacer.vydejZvuk();
 
-const kohout = new Kohout('kikiriki');
+// const kohout = new Kohout('kikiriki');
 
-kohout.vydejZvuk();
+// kohout.vydejZvuk();
