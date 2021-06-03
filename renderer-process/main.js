43b0dfeaa311ca9711c8cdea861c6356fef035ef
Peter Palka <softbyteavocado@gmail.com>
2021-06-03 19:56:28 +0200

    konec devate lekce

--- renderer-process/main.js
+++ renderer-process/main.js
@@ -42,11 +42,38 @@ function showDayModal() {
 
     document.body.appendChild(modal);
 
+    const checkbox = document.querySelector('#limitAttendeesByGender');
+    const row = document.querySelector("#genderSelectRow");
+    checkbox.addEventListener('change', (event) => {
+        if (event.target.checked) {
+            row.classList.remove('hidden');
+        } else {
+            row.classList.add('hidden');
+        }
+    });
+
+    // pri vytvarani options pridat triedu
+    // ziskat referencie na vsetky elementy s nasou novou triedou
+    // querySelectorAll
+    // select.removeChild(it)
+    // document.removeChild(it)
+
+    debugger;
+    const days = document.querySelectorAll('app-day');
+
+    const daysArray = Array.from(days);
+
+    for (const item of days) {
+        console.log(item);
+    }
+
+
     fetch('http://localhost:3000/contacts')
         .then(serverResponse => serverResponse.text())
         .then(responseText => {
         const data = JSON.parse(responseText);
         const select = document.querySelector('#eventAttendees');
+
         data.forEach(it => {
             const option = document.createElement('option');
             option.setAttribute('value', it.id);
@@ -79,3 +106,22 @@ fetch('http://localhost:3000/contacts')
 
       // vlozit option do selectu
     })
+
+    const exampleArray = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    exampleArray.forEach(it => {
+        console.log(it);
+    });
+
+    const rlt = exampleArray.map(it => {
+        return it + 1;
+    });
+
+    console.log(rlt);
+
+    const rlt2 = exampleArray.filter(it => {
+        const isEven = it % 2 === 0;
+        return isEven;
+    });
+
+    console.log(rlt2);
